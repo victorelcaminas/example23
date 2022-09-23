251350f24811f684a96ffc48eb4a66f453bf9a5a
rvt <alutoa2411@ieselcaminas.org>
2022-09-23 17:16:18 +0200

    Initial commit

---
+++ src/Example23.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Example23 {
+    public static void main(String[] args){
+        float grade = 0, sum = 0;
+        boolean wasTen = false;
+        int i = 0;
+
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+
+        while ( grade != -1) {
+            grade = inputValue.nextFloat();
+            if (grade != -1) {
+                sum = sum + grade;
+                    i = i + 1;
+                    if (grade == 10){
+                        wasTen = true;
+                }
+            }
+        }
+        System.out.println("Average: " +sum / i);
+        if (wasTen) {
+            System.out.println("There was a 10");
+        } else {
+            System.out.println("There was not a 10");
+        }
+    }
+}
